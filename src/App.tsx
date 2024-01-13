458080371be6d7f44f912ecf716d3865712b1987
GustavoG18 <gguerrerof98@gmail.com>
2024-01-12 23:33:09 -0500

    feat: :lipstick: Tailwindcss was configured to the project. Added two icons for use in the StatusCard component, to express the statuses of the API request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
+import { StatusCard } from './components/StatusCard/StatusCard';
+import { API_NAMES_LIST } from './Utils/api-names-list';
 
 const App = () => {
-  return <h1>Hola Mundo</h1>
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {API_NAMES_LIST.map((name: string, index) => {
+          return <StatusCard key={index} apiName={name} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default App;
